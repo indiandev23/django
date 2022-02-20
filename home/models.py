45c9d04455d35ecbb866ebe5c13190f96d941cab
Prince Tiwari <92683922+indiandev23@users.noreply.github.com>
2022-02-20 21:50:47 +0530

    first commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+#from email.headerregistry import Address
 from django.db import models
+#from django.forms import PasswordInput
 
 # Create your models here.
+class Contact(models.Model):
+    Name= models.CharField(max_length=122)
+    Email= models.CharField(max_length=122)
+    Password= models.CharField(max_length=122)
+    Address= models.CharField(max_length=122)
+
+    
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -14,4 +15,11 @@ def services(request):
     return render(request, 'services.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        contact= Contact(name=name, email=email, password=password, address=address)
+        contact.save()
     return render(request, 'contact.html')
